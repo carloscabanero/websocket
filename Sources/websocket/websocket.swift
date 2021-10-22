ae730ead26424aef5abe47953960714b4b367235
Carlos Cabanero <carlos@blink.sh>
2021-10-21 21:56:26 -0400

    Basics

--- Sources/websocket/websocket.swift
+++ Sources/websocket/websocket.swift
@@ -1,6 +1,52 @@
-public struct websocket {
-    public private(set) var text = "Hello, World!"
+import Network
+import Foundation
 
-    public init() {
+public class BKWebSocketServer {
+    var listener: NWListener!
+    let queue = DispatchQueue(label: "WebSocketServer")
+    
+    public init() throws {
+        try startListening()
+    }
+    
+    // TODO Port
+    func startListening() throws {
+        let parameters = NWParameters.tcp
+        let websocketOptions = NWProtocolWebSocket.Options()
+        parameters.defaultProtocolStack.applicationProtocols.insert(websocketOptions, at: 0)
+        
+        self.listener = try NWListener(using: parameters, on: 8000)
+        
+        listener.newConnectionHandler = { [weak self] in self?.handleNewConnection($0) }
+        // TODO Review the state
+        listener.stateUpdateHandler = { print("WebSocket Listener \($0)") }
+        listener.start(queue: queue)
+    }
+    
+    func handleNewConnection(_ conn: NWConnection) {
+        // TODO Skipping on the statehandler for the connection for now
+        //connections.append(conn)
+        conn.start(queue: queue)
+        receiveNextMessage(conn)
+    }
+    
+    func receiveNextMessage(_ conn: NWConnection) {
+        conn.receiveMessage { (content, context, isComplete, error) in
+            if let data = content, let context = context {
+                let reply = self.handleMessage(data: data, context: context)
+                let metadata = NWProtocolWebSocket.Metadata(opcode: .binary)
+                let context = NWConnection.ContentContext(identifier: "binaryContext",
+                                                          metadata: [metadata])
+                
+                conn.send(content: reply, contentContext: context, completion: .idempotent)
+                self.receiveNextMessage(conn)
+            }
+        }
+    }
+    
+    func handleMessage(data: Data, context: NWConnection.ContentContext) -> Data {
+        // TODO We may need an additional Framer for our protocol, specially if we are
+        // sending data in chunks (maybe for big files?).
+        return data
     }
 }
--- Tests/websocketTests/websocketTests.swift
+++ Tests/websocketTests/websocketTests.swift
@@ -1,11 +1,42 @@
 import XCTest
+import Network
+
+
 @testable import websocket
 
 final class websocketTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(websocket().text, "Hello, World!")
+    func testWebSocket() throws {
+      let message = "Hello World"
+      let expectation = XCTestExpectation(description: "Message received")
+
+      // Start webserver. Wait for it to be ready. We will create a proper ready flag.
+      let server = try BKWebSocketServer()
+      RunLoop.current.run(until: Date.init(timeIntervalSinceNow: 1))
+        
+      let task = URLSession.shared.webSocketTask(with: URL(string: "ws://localhost:8000")!)
+      task.resume()
+
+        task.send(.string(message)) { error in if let error = error { XCTFail("\(error)") } }
+      task.receive { result in
+          expectation.fulfill()
+
+          switch result {
+          case .success(let result):
+              var msg: String? = nil
+              switch result {
+              case .data(let data):
+                msg = String(data: data, encoding: .utf8)
+              case .string(let str):
+                msg = str
+              @unknown default:
+                  XCTFail("Unknown")
+              }
+              XCTAssert(msg == message)
+          case .failure(let error):
+              XCTFail("\(error)")
+          }
+      }
+
+      wait(for: [expectation], timeout: 2.0)
     }
 }
